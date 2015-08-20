0180c03da75722eb79c3d7b2daa4a82c9dfa74ac
Rafael Pires <orafa.pires@gmail.com>
2015-08-20 10:58:13 -0300

    Mais alguns ajustes...

--- assets-server.sh
+++ assets-server.sh
@@ -6,6 +6,8 @@ cont_httpd="httpd"
 port_vm_httpd=80
 cont_samba="appcontainers/samba"
 name_cont_samba="samba"
+smbuser="admin"
+smbpass="password"
 path_vm="/var/assets"
 
 # Instalação do assets server
@@ -14,23 +16,27 @@ if which $programname >/dev/null; then
 	docker pull $cont_httpd
 	docker pull $cont_samba
 	# Executar o container do Apache montando o diretorio a ser persistido na maquina virtual/fisica
-	docker run -d -it --name $cont_httpd -h $cont_httpd -p $port_vm_httpd:80 -v $path_vm:/var/www/html $cont_httpd
+	docker run -d -it --name $cont_httpd -h $cont_httpd -p $port_vm_httpd:80 -v $path_vm:/var/www/html $cont_httpd >/dev/null 2>&1
 	# Checar se o container está rodando
 	cont_httpd_status=$(docker inspect --format="{{ .State.Running }}" $cont_httpd)
 		if $cont_httpd_status == "true"; then
-			echo "Container rodando"
+			echo "Container $cont_httpd rodando"
 		else
-			echo "Container parado"
+			echo "Container $cont_httpd parado"
+			cont_httpd_status=$(docker inspect --format="{{ .State.Error }}" $cont_httpd)
+			echo "Verificar erro:" $cont_httpd_status
 		fi
 	# Executar o container do Samba montando o diretorio do Apache como compartilhamento do serviço
-	docker run -d -it --name $name_cont_samba -h $name_cont_samba -p 138:138/udp -p 139:139 -p 445:445 -p 445:445/udp --volumes-from $cont_httpd -e SMB_USER='admin' -e SMB_PASS='password' $cont_samba
+	docker run -d -it --name $name_cont_samba -h $name_cont_samba -p 138:138/udp -p 139:139 -p 445:445 -p 445:445/udp --volumes-from $cont_httpd -e SMB_USER='$smbuser' -e SMB_PASS='$smbpass' $cont_samba >/dev/null 2>&1
 	# Checar se o container está rodando
 	cont_samba_status=$(docker inspect --format="{{ .State.Running }}" $name_cont_samba)
 		if $cont_samba_status == "true"; then
-                        echo "Container rodando"
+                        echo "Container $name_cont_samba rodando"
                 else
-                        echo "Container parado"
-                fi
+                        echo "Container $name_cont_samba parado"
+			cont_samba_status=$(docker inspect --format="{{ .State.Error }}" $name_cont_samba)
+                	echo "Verificar erro:" $cont_samba_status
+		fi
 else
     echo "Docker não encontrado"
 fi
